ae7e32af91e14923cf2474bc234430f4f70cce86
Furkan Kaptan <iamfurkan86@gmail.com>
2025-02-10 16:38:47 +0300

    EmailAPI EmailController and "send" endpoint created.

---
+++ PortfolioApp/PortfolioApp.EmailAPI/Controllers/EmailController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioApp.Core.DTOs.Email;
+
+namespace PortfolioApp.EmailAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmailController : ControllerBase
+{
+    [HttpPost("send")]
+    public async Task<IActionResult> SendEmailAsync([FromBody] EmailRequestDto emailRequest)
+    {
+        return Ok();
+    }
+}
