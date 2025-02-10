d0df9487eff42aaa151480985ccb6b01cb7b45bb
Furkan Kaptan <iamfurkan86@gmail.com>
2025-02-10 16:48:28 +0300

    EmailAPI EmailController  "send" endpoint implemented.

--- PortfolioApp/PortfolioApp.EmailAPI/Controllers/EmailController.cs
+++ PortfolioApp/PortfolioApp.EmailAPI/Controllers/EmailController.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PortfolioApp.Core.DTOs.Email;
+using PortfolioApp.Core.Interfaces;
 
 namespace PortfolioApp.EmailAPI.Controllers;
 
@@ -8,9 +8,21 @@ namespace PortfolioApp.EmailAPI.Controllers;
 [ApiController]
 public class EmailController : ControllerBase
 {
+    private readonly IEmailService _emailService;
+
+    public EmailController(IEmailService emailService)
+    {
+        _emailService = emailService;
+    }
+    
     [HttpPost("send")]
     public async Task<IActionResult> SendEmailAsync([FromBody] EmailRequestDto emailRequest)
     {
-        return Ok();
+        var result = await _emailService.SendEmailAsync(emailRequest);
+
+        if (result)
+            return Ok("Email başarıyla gönderildi.");
+
+        return StatusCode(500,"Email gönderilirken bir hata oluştu!..");
     }
 }
