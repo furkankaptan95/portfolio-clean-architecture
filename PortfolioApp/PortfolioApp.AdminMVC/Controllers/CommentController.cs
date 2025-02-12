4e0315ecbcb7fc3642ee65e569a19df1692a0b9a
Furkan Kaptan <iamfurkan86@gmail.com>
2025-02-12 20:02:29 +0300

    AdminMVC CommentController All action implemented.

---
+++ PortfolioApp/PortfolioApp.AdminMVC/Controllers/CommentController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioApp.AdminMVC.Models.ViewModels.Comment;
+using PortfolioApp.Core.Interfaces;
+
+namespace PortfolioApp.AdminMVC.Controllers;
+public class CommentController : Controller
+{
+    private readonly ICommentService _commentService;
+    private readonly IMapper _mapper;
+    public CommentController(ICommentService commentService, IMapper mapper)
+    {
+        _commentService = commentService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> All()
+    {
+        
+        var result = await _commentService.GetAllAsync();
+            
+        var models = _mapper.Map<List<CommentViewModel>>(result.Data);
+
+        return View(models);
+                
+    }
+}
---
+++ PortfolioApp/PortfolioApp.AdminMVC/Services/CommentService.cs
@@ -0,0 +1,36 @@
+﻿using PortfolioApp.Core.Common;
+using PortfolioApp.Core.DTOs.Admin.Comment;
+using PortfolioApp.Core.DTOs.Web.Comment;
+using PortfolioApp.Core.Interfaces;
+
+namespace PortfolioApp.AdminMVC.Services;
+public class CommentService : ICommentService
+{
+    private readonly IHttpClientFactory _factory;
+    public CommentService(IHttpClientFactory factory)
+    {
+        _factory = factory;
+    }
+    private HttpClient DataApiClient => _factory.CreateClient("dataApi");
+    public Task<ServiceResult> AddAsync(AddCommentDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ServiceResult> ApprovalAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ServiceResult> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ServiceResult<List<CommentDto>>> GetAllAsync()
+    {
+        var apiResponse = await DataApiClient.GetAsync("comment/all");
+
+        return await apiResponse.Content.ReadFromJsonAsync<ServiceResult<List<CommentDto>>>();
+    }
+}
