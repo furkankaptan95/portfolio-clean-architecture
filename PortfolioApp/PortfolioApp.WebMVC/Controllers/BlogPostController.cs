9efeb34dbeb38c52554086220758e6b773ed44ab
Furkan Kaptan <iamfurkan86@gmail.com>
2025-02-13 17:27:46 +0300

    WebMVC BlogPost view and _BlogLayout integrated

--- PortfolioApp/PortfolioApp.WebMVC/Controllers/BlogPostController.cs
+++ PortfolioApp/PortfolioApp.WebMVC/Controllers/BlogPostController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PortfolioApp.Core.Interfaces;
+using PortfolioApp.WebMVC.Models.ViewModels;
 
 namespace PortfolioApp.WebMVC.Controllers;
 public class BlogPostController : Controller
@@ -11,5 +12,25 @@ public class BlogPostController : Controller
     {
         _blogPostService = blogPostService;
         _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> BlogPost([FromRoute] int id)
+    {
+        if (id < 1)
+        {
+            TempData["ErrorMessage"] = "BlogPost bulunamadı.";
+            return Redirect("/all-blog-posts");
+        }
+
+        var model = new BlogPostViewModel
+        {
+            Id = id,
+            Title = "Title",
+            Content = "Content",
+            PublishDate = DateTime.Now,
+        };
+
+        return View(model);
     }
 }
