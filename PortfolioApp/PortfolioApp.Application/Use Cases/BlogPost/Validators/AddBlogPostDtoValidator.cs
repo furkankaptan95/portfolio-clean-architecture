5b09f9890030d8a482c15c9d68d4d559af4892d5
Furkan Kaptan <iamfurkan86@gmail.com>
2025-02-14 22:49:54 +0300

    DataAPI BlogPost Validators added.

---
+++ PortfolioApp/PortfolioApp.Application/Use Cases/BlogPost/Validators/AddBlogPostDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using PortfolioApp.Core.DTOs.Admin.BlogPost;
+
+namespace PortfolioApp.Application.Use_Cases.BlogPost.Validators;
+
+public class AddBlogPostDtoValidator : AbstractValidator<AddBlogPostDto>
+{
+    public AddBlogPostDtoValidator()
+    {
+        RuleFor(x => x.Title)
+           .Must(name => !string.IsNullOrWhiteSpace(name)).WithMessage("Başlık kısmı boş olamaz.")
+            .MaximumLength(100).WithMessage("Başlık maksimum 100 karakter olabilir.");
+
+        RuleFor(x => x.Content)
+           .Must(name => !string.IsNullOrWhiteSpace(name)).WithMessage("İçerik kısmı boş olamaz.");
+
+    }
+}
