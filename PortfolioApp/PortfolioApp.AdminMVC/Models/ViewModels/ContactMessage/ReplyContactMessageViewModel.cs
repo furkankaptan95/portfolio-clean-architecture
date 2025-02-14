10e6898966551f4ae435d8709e0f2f2b56663cb9
Furkan Kaptan <iamfurkan86@gmail.com>
2025-02-15 02:53:42 +0300

    AdminMVC ContactMessage  DataAnnotations and Error Message added.

--- PortfolioApp/PortfolioApp.AdminMVC/Models/ViewModels/ContactMessage/ReplyContactMessageViewModel.cs
+++ PortfolioApp/PortfolioApp.AdminMVC/Models/ViewModels/ContactMessage/ReplyContactMessageViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace PortfolioApp.AdminMVC.Models.ViewModels.ContactMessage;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioApp.AdminMVC.Models.ViewModels.ContactMessage;
 public class ReplyContactMessageViewModel
 {
+    [Required(ErrorMessage = "Id 0'dan büyük olmalıdır.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Id 0'dan büyük olmalıdır.")]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Mesaj kısmı boş olamaz.")]
     public string Message { get; set; }
 }
