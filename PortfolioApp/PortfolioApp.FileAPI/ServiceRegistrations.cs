a4a5837947ed13072f2648c319bba12b843724f2
Furkan Kaptan <iamfurkan86@gmail.com>
2025-02-16 01:49:02 +0300

    FileAPI -> A control mechanism has been added for the Bearer token that comes with the request.

--- PortfolioApp/PortfolioApp.FileAPI/ServiceRegistrations.cs
+++ PortfolioApp/PortfolioApp.FileAPI/ServiceRegistrations.cs
@@ -1,6 +1,10 @@
-﻿using PortfolioApp.Application.Business_Logic.Services;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using PortfolioApp.Application.Business_Logic.Services;
 using PortfolioApp.Application.Common.Configurations;
+using PortfolioApp.Core.Common;
 using PortfolioApp.Core.Interfaces;
+using System.Text;
 
 namespace PortfolioApp.FileAPI;
 public static class ServiceRegistrations
@@ -18,8 +22,8 @@ public static class ServiceRegistrations
         });
 
         services.AddControllers();
-
-        services.AddEndpointsApiExplorer();
+		AddJwtAuth(services, configuration);
+		services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
 
         services.Configure<FileSettings>(configuration.GetSection("FileSettings"));
@@ -27,4 +31,60 @@ public static class ServiceRegistrations
 
         return services;
     }
+
+	private static void AddJwtAuth(IServiceCollection services, IConfiguration configuration)
+	{
+		var tokenOptions = configuration.GetSection("Jwt").Get<JwtTokenOptions>();
+
+		services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+		.AddJwtBearer(options =>
+		{
+			options.TokenValidationParameters = new TokenValidationParameters
+			{
+				ValidIssuer = tokenOptions.Issuer,
+				ValidateIssuer = true,
+				ValidAudience = tokenOptions.Audience,
+				ValidateAudience = true,
+				ValidateLifetime = true,
+				IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenOptions.Key)),
+				ValidateIssuerSigningKey = true,
+				ClockSkew = TimeSpan.Zero
+			};
+
+			options.MapInboundClaims = true;
+
+			options.Events = new JwtBearerEvents
+			{
+				OnMessageReceived = context =>
+				{
+					var accessToken = context.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+
+					if (!string.IsNullOrEmpty(accessToken))
+					{
+						context.Token = accessToken;
+					}
+
+					return Task.CompletedTask;
+				},
+
+				OnChallenge = async context =>
+				{
+					// Token geçersiz olduğu için 401 döndürüyoruz
+					context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+					context.Response.ContentType = "application/json";
+					await context.Response.WriteAsync("{\"error\": \"Unauthorized. Please login.\"}");
+
+					context.HandleResponse(); // Yanıt tamamlandı, isteği durdur
+				},
+
+				OnForbidden = context =>
+				{
+					// Yetki sorunu varsa 403 Forbidden dön
+					context.Response.StatusCode = StatusCodes.Status403Forbidden;
+					context.Response.ContentType = "application/json";
+					return context.Response.WriteAsync("{\"error\": \"Forbidden. You do not have access to this resource.\"}");
+				}
+			};
+		});
+	}
 }
