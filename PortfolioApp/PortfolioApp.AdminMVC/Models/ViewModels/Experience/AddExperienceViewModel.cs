25146848a12566c8fe90ae19c4fd936cfd710243
Furkan Kaptan <iamfurkan86@gmail.com>
2025-02-15 03:21:07 +0300

    AdminMVC Experience DataAnnotations added.

--- PortfolioApp/PortfolioApp.AdminMVC/Models/ViewModels/Experience/AddExperienceViewModel.cs
+++ PortfolioApp/PortfolioApp.AdminMVC/Models/ViewModels/Experience/AddExperienceViewModel.cs
@@ -1,9 +1,25 @@
-﻿namespace PortfolioApp.AdminMVC.Models.ViewModels.Experience;
+﻿using PortfolioApp.AdminMVC.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioApp.AdminMVC.Models.ViewModels.Experience;
 public class AddExperienceViewModel
 {
+    [Required(ErrorMessage = "Başlık kısmı boş olamaz.")]
+    [StringLength(100, ErrorMessage = "Başlık maksimum 100 karakter olabilir.")]
     public string Title { get; set; }
+
+    [Required(ErrorMessage = "Şirket kısmı boş olamaz.")]
+    [StringLength(100, ErrorMessage = "Şirket maksimum 100 karakter olabilir.")]
     public string Company { get; set; }
+
+    [Required(ErrorMessage = "Açıklama kısmı boş olamaz.")]
     public string Description { get; set; }
+
+    [Required(ErrorMessage = "Başlangıç tarihi gereklidir.")]
+    [DataType(DataType.Date, ErrorMessage = "Geçerli bir tarih olmalı.")]
     public DateTime StartDate { get; set; }
+
+    [DataType(DataType.Date, ErrorMessage = "Geçerli bir tarih olmalı.")]
+    [EndDateAfterStartDate(nameof(StartDate), ErrorMessage = "Bitiş tarihi, başlangıç tarihinden sonra olmalı.")]
     public DateTime? EndDate { get; set; }
 }
