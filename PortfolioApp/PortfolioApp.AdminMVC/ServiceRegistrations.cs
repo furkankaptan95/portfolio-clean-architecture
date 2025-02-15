5c8bd5467aa055535bd3788a86fc9f79ac2c4c83
Furkan Kaptan <iamfurkan86@gmail.com>
2025-02-15 18:25:30 +0300

    Moved service registrations in AdminMVC Program.cs to ServiceRegistrations class.

---
+++ PortfolioApp/PortfolioApp.AdminMVC/ServiceRegistrations.cs
@@ -0,0 +1,51 @@
+﻿using PortfolioApp.AdminMVC.Mappers;
+using PortfolioApp.AdminMVC.Service;
+using PortfolioApp.AdminMVC.Services;
+using PortfolioApp.Core.Config;
+using PortfolioApp.Core.Interfaces;
+
+namespace PortfolioApp.AdminMVC;
+public static class ServiceRegistrations
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllersWithViews();
+
+        services.Configure<FileApiSettings>(configuration.GetSection("FileApiSettings"));
+
+        var dataApiUrl = configuration.GetValue<string>("DataApiUrl");
+        if (string.IsNullOrWhiteSpace(dataApiUrl))
+        {
+            throw new InvalidOperationException("DataApiUrl is required in appsettings.json");
+        }
+        services.AddHttpClient("dataApi", c =>
+        {
+            c.BaseAddress = new Uri(dataApiUrl);
+        });
+
+        var fileApiUrl = configuration.GetValue<string>("FileApiUrl");
+        if (string.IsNullOrWhiteSpace(fileApiUrl))
+        {
+            throw new InvalidOperationException("FileApiUrl is required in appsettings.json");
+        }
+        services.AddHttpClient("fileApi", c =>
+        {
+            c.BaseAddress = new Uri(fileApiUrl);
+        });
+
+        services.AddScoped<HomeService>();
+        services.AddScoped<IAboutMeService, AboutMeService>();
+        services.AddScoped<IBlogPostService, BlogPostService>();
+        services.AddScoped<IEducationService, EducationService>();
+        services.AddScoped<IExperienceService, ExperienceService>();
+        services.AddScoped<IPersonalInfoService, PersonalInfoService>();
+        services.AddScoped<IProjectService, ProjectService>();
+        services.AddScoped<ICommentService, CommentService>();
+        services.AddScoped<IContactMessageService, ContactMessageService>();
+
+        services.AddAutoMapper(typeof(ViewModelMappingProfile));
+
+
+        return services;
+    }
+}
