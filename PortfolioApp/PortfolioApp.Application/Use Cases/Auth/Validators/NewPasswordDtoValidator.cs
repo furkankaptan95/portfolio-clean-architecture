09adfc3f18eebefa450a4fa81d1c59fd8b8311a2
Furkan Kaptan <iamfurkan86@gmail.com>
2025-02-17 17:21:02 +0300

    AuthDto Validators added.

---
+++ PortfolioApp/PortfolioApp.Application/Use Cases/Auth/Validators/NewPasswordDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using PortfolioApp.Core.DTOs.Auth;
+
+namespace PortfolioApp.Application.Use_Cases.Auth.Validators;
+public class NewPasswordDtoValidator : AbstractValidator<NewPasswordDto>
+{
+    public NewPasswordDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .Must(name => !string.IsNullOrWhiteSpace(name)).WithMessage("Email alanı boş bırakılamaz.")
+            .EmailAddress().WithMessage("Geçerli bir email adresi giriniz.")
+            .MaximumLength(100).WithMessage("Email 100 karakterden uzun olamaz.");
+
+        RuleFor(x => x.Token)
+            .Must(name => !string.IsNullOrWhiteSpace(name)).WithMessage("Token alanı boş bırakılamaz.");
+
+        RuleFor(x => x.Password)
+            .Must(name => !string.IsNullOrWhiteSpace(name)).WithMessage("Şifre alanı boş bırakılamaz.")
+            .MinimumLength(8).WithMessage("Şifre en az 8 karakter olmalıdır.")
+            .MaximumLength(15).WithMessage("Şifre en fazla 15 karakter olabilir.");
+    }
+}
---
+++ PortfolioApp/PortfolioApp.Application/Use Cases/Auth/Validators/NewVerificationMailDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using PortfolioApp.Core.DTOs.Auth;
+
+namespace PortfolioApp.Application.Use_Cases.Auth.Validators;
+public class NewVerificationMailDtoValidator : AbstractValidator<NewVerificationMailDto>
+{
+    public NewVerificationMailDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .Must(name => !string.IsNullOrWhiteSpace(name)).WithMessage("Email alanı boş bırakılamaz.")
+            .EmailAddress().WithMessage("Geçerli bir email adresi giriniz.")
+            .MaximumLength(100).WithMessage("Email 100 karakterden uzun olamaz.");
+    }
+}
