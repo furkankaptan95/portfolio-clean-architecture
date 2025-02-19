0060187f087c8534352e44eb2042381bd985deca
Furkan Kaptan <iamfurkan86@gmail.com>
2025-02-19 19:36:36 +0300

    Update ReplyContactMessageHandler.cs
    Contact Message The function of automatically sending an e-mail to the response given during the reply process has been added.

--- PortfolioApp/PortfolioApp.Application/Use Cases/ContactMessage/Handlers/ReplyContactMessageHandler.cs
+++ PortfolioApp/PortfolioApp.Application/Use Cases/ContactMessage/Handlers/ReplyContactMessageHandler.cs
@@ -2,16 +2,22 @@
 using Microsoft.EntityFrameworkCore;
 using PortfolioApp.Application.Use_Cases.ContactMessage.Commands;
 using PortfolioApp.Core.Common;
+using PortfolioApp.Core.DTOs.Email;
 using PortfolioApp.Infrastructure.Persistence.DbContexts;
+using System.Net.Http.Json;
 
 namespace PortfolioApp.Application.Use_Cases.ContactMessage.Handlers;
 public class ReplyContactMessageHandler : IRequestHandler<ReplyContactMessageCommand, ServiceResult>
 {
+    private readonly IHttpClientFactory _factory;
     private readonly DataDbContext _dataDbContext;
-    public ReplyContactMessageHandler(DataDbContext dataDbContext)
+    public ReplyContactMessageHandler(DataDbContext dataDbContext, IHttpClientFactory factory)
     {
         _dataDbContext = dataDbContext;
+        _factory = factory;
     }
+
+    private HttpClient EmailApiClient => _factory.CreateClient("emailApi");
     public async Task<ServiceResult> Handle(ReplyContactMessageCommand request, CancellationToken cancellationToken)
     {
         var entity = await _dataDbContext.ContactMessages.FirstOrDefaultAsync(x => x.Id == request.ReplyDto.Id);
@@ -27,6 +33,17 @@ public class ReplyContactMessageHandler : IRequestHandler<ReplyContactMessageCom
 
         _dataDbContext.ContactMessages.Update(entity);
         await _dataDbContext.SaveChangesAsync(cancellationToken);
+
+        var htmlMailBody = $"<h1>Merhaba, Ben Furkan!</h1><p>{entity.Reply}</p>";
+
+        var emailRequest = new EmailRequestDto
+        {
+            Body = htmlMailBody,
+            Subject = "Benimle iletişime geçtiğiniz için teşekür ederim. İşte yanıtım!",
+            To = entity.Email,
+        };
+
+        var emailResult = await EmailApiClient.PostAsJsonAsync("send", emailRequest);
 
         return new ServiceResult(true, "Mesaj başarıyla yanıtlandı.");
     }
